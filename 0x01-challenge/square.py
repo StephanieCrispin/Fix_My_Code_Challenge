74048d3db4020b9162fd2592efdd0e9ec77a1a0f
StephanieMfon <okpomfonstephanie@gmail.com>
2024-02-05 00:32:37 +0100

    added changes

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,6 +7,7 @@ class square():
 
     
     def __init__(self, *args, **kwargs):
+        """Instantiate a class"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,15 +15,17 @@ class square():
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """Perimeter of my square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Printable representation"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+"""Create a square object"""
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
